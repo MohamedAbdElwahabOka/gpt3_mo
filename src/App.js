90903a6ea11433a48ffad537efc6359cff4e2d05
MohamedAbdElwahabOka <boka22986@gmail.com>
2023-06-23 07:20:13 +0300

    add all components files and folders

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
     return (
         <div>
-            <h1>React App</h1>
+            <h1>gpt 3</h1>
 
         </div>
     )
---
+++ src/components/cta/Cat.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './cat.css'
+function Cat() {
+  return (
+    <div>
+      Cat
+    </div>
+  )
+}
+
+export default Cat
